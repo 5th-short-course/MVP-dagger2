227b883244caead3d5dc74938c791a3429005920
Rathana Voy <rathanavoy07@gmail.com>
2018-09-29 11:50:25 +0700

    init project demo MVP and dagger2

---
+++ app/src/main/java/com/example/androidhrd/mvp_demo/data/repo/Database.java
@@ -0,0 +1,16 @@
+package com.example.androidhrd.mvp_demo.data.repo;
+
+public class Database {
+    private static  Database database;
+    private Database(){}
+    public static Database getDatabase() {
+        if(database==null)
+            database=new Database();
+        return database;
+    }
+
+    public UserRepository getUserRepository(){
+        return new UserRepository();
+    }
+
+}
---
+++ app/src/main/java/com/example/androidhrd/mvp_demo/login/mvp/Presenter.java
@@ -0,0 +1,49 @@
+package com.example.androidhrd.mvp_demo.login.mvp;
+
+import com.example.androidhrd.mvp_demo.entity.User;
+
+public class Presenter implements LoginMvp.Presenter {
+    //View object
+    private LoginMvp.View view;
+    //Interactor object
+    LoginMvp.Interactor interactor;
+    public Presenter(){
+        interactor=new Interactor();
+    }
+    @Override
+    public void setView(LoginMvp.View view) {
+        this.view=view;
+    }
+
+    @Override
+    public void login(User user) {
+        //request to Interactor
+        if(view!=null)
+            view.onShowLoading();
+        interactor.authenticate(user,response);
+    }
+
+    private LoginMvp.Interactor.InteratorResponse response=
+            new LoginMvp.Interactor.InteratorResponse() {
+                @Override
+                public void onLoginSuccess(User user) {
+                   if(view!=null){
+                       view.onLoginSuccess(user);
+                       view.onHideLoading();
+                   }
+                }
+
+                @Override
+                public void onFailLogin(String smg) {
+                    if(view!=null){
+                        view.onFailLogin(smg);
+                        view.onHideLoading();
+                    }
+                }
+
+                @Override
+                public void OnError(String error) {
+                    //no need to call
+                }
+            };
+}
